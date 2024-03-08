861c5ad679c675250a36d68ff20bbf15b8169301
aslan316 <ljhord31@gmail.com>
2024-03-08 11:24:14 -0500

    Add function playRound(String, String)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,3 +9,34 @@ function getComputerChoice() {
             return "SCISSORS";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "ROCK":
+            switch (computerSelection) {
+                case "ROCK":
+                    return "You tie! Rock ties Rock";
+                case "PAPER":
+                    return "You lose! Paper beats Rock";
+                case "SCISSORS":
+                    return "You win! Rock beats Scissors";
+            }
+        case "PAPER":
+            switch (computerSelection) {
+                case "ROCK":
+                    return "You win! Paper beats Rock";
+                case "PAPER":
+                    return "You tie! Paper ties Paper";
+                case "SCISSORS":
+                    return "You lose! Scissors beats Paper";
+            }
+        case "SCISSORS":
+            switch (computerSelection) {
+                case "ROCK":
+                    return "You lose! Rock beats Scissors";
+                case "PAPER":
+                    return "You win! Scissors beats Paper";
+                case "SCISSORS":
+                    return "You tie! Scissors tie Scissors";
+            }
+    }
