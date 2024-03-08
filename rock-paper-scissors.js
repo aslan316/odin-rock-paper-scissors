1bac747b53253aacfe44a21462565dae22102e60
aslan316 <ljhord31@gmail.com>
2024-03-08 11:36:09 -0500

    Add function playGame(n) and call it

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,13 @@
+playGame(5);
+
+function playGame(n) {
+    for (let i = 0; i < n; i++) {
+        const playerSelection = prompt("What will you play: ").toUpperCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
     switch (randomChoice) {
